79ec6ebe71232f4076c4e1673532449a375ece81
Francesco <franco.labate19@gmail.com>
2025-02-18 11:54:25 +0100

    Navbar Update, add logo placeholder

--- blog/src/components/navbar.js
+++ blog/src/components/navbar.js
@@ -1,20 +1,25 @@
 import style from "./navbar.module.css";
 import Link from "next/link";
+import logo from "../../public/logo/logo.png";
+import Image from "next/image";
 
 export default function Navbar() {
   return (
     <nav className={style.nav}>
+      <Link href="/" className={style.logo}>
+        <Image src={logo} alt="main page logo" width={90} height={90} />
+      </Link>
       <ul className={style.navlist}>
         <li className={style.navlink}>
           <Link href="/">Home</Link>
         </li>
-        <li className={style.link}>
+        <li className={style.navlink}>
           <Link href="/about">About us</Link>
         </li>
-        <li className={style.link}>
+        <li className={style.navlink}>
           <Link href="/blog">Blog</Link>
         </li>
-        <li className={style.link}>
+        <li className={style.navlink}>
           <Link href="/contacts">Contact us</Link>
         </li>
       </ul>
